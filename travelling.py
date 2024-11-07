c8919146ac43aa023574aa6d3ef005dc7bfbf427
Swetha P V <swethapv03@gmail.com>
2024-11-07 20:27:09 +0530

    Update travelling.py

--- travelling.py
+++ travelling.py
@@ -1,21 +1,35 @@
 from sys import maxsize
 from itertools import permutations
-V=4
-def travellingSalesmanProblem(graph,s):
-vertex=[]
-for i in range(V):
-if i!=s:
-vertex.append(i)
-min_path=maxsize
-next_permutations=permutations(vertex)
-for i in next_permutation:
-current_pathweight=0
-k=s
-for j in i:
-current_pathweight+=graph[k][s]
-min_path=min(min_path,current_pathweight)
-return min_path
-if__name__=="__main__":
-graph=[[0,10,15,20],[10,0,35,25],[15,35,0,30],[20,25,30,0]]
-s=0
-print(travellingSalesmanProblem(graph,s))
+
+V = 4
+
+def travellingSalesmanProblem(graph, s):
+    vertex = []
+    for i in range(V):
+        if i != s:
+            vertex.append(i)
+
+    min_path = maxsize
+    next_permutations = permutations(vertex)
+    
+    for i in next_permutations:
+        current_pathweight = 0
+        k = s
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+        
+        min_path = min(min_path, current_pathweight)
+    
+    return min_path
+
+if __name__ == "__main__":
+    graph = [
+        [0, 10, 15, 20],
+        [10, 0, 35, 25],
+        [15, 35, 0, 30],
+        [20, 25, 30, 0]
+    ]
+    s = 0
+    print(travellingSalesmanProblem(graph, s))
